fc546a98ba2ceabde454a33d16befc32b8b8914d
Jihun Yu <30788586+wlgns12370@users.noreply.github.com>
2024-10-26 18:25:28 +0900

    3.4.3.3 요구사항 3 - POST 방식으로 회원가입하기 (#3)
    * feat: post 방식으로 회원가입
    
    * test: post 방식으로 회원가입 테스트
    
    * docs: ch3 요구사항3 README.md 작성

--- src/test/java/webserver/entity/RequestEntityTest.java
+++ src/test/java/webserver/entity/RequestEntityTest.java
@@ -11,13 +11,35 @@ import org.junit.Test;
 public class RequestEntityTest {
     
     @Test
-    public void GET_방식으로_회원가입_할_수_있다() throws IOException{
+    public void GET_방식으로_회원가입_할_수_있다() throws IOException {
         final String input = "GET /user/create?userId=userId&password=password&name=name&email=wlgns12370%gmail.com HTTP/1.1";
         InputStream in = new ByteArrayInputStream(input.getBytes());
 
         RequestEntity requestEntity = RequestEntity.from(in);
 
-        assertEquals(HttpMethod.GET, requestEntity.getHttpMessage());
+        assertEquals(HttpMethod.GET, requestEntity.getHttpMethod());
+        assertEquals("/user/create", requestEntity.getUri());
+
+        assertEquals("userId", requestEntity.getQueryString().get("userId"));
+        assertEquals("password", requestEntity.getQueryString().get("password"));
+        assertEquals("name", requestEntity.getQueryString().get("name"));
+        assertEquals("wlgns12370%gmail.com", requestEntity.getQueryString().get("email"));
+    }
+
+    @Test
+    public void POST_방식으로_회원가입_할_수_있다() throws IOException {
+        String bodyData = "userId=userId&password=password&name=name&email=wlgns12370%gmail.com";
+        
+        String input = "POST /user/create HTTP/1.1\r\n" +
+                    "content-length: " + bodyData.length() + "\r\n" +
+                    "\r\n" +
+                    bodyData;
+                        
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+
+        RequestEntity requestEntity = RequestEntity.from(in);
+
+        assertEquals(HttpMethod.POST, requestEntity.getHttpMethod());
         assertEquals("/user/create", requestEntity.getUri());
 
         assertEquals("userId", requestEntity.getQueryString().get("userId"));
