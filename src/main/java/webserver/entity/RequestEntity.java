12480081cac3ffcb8e6c5053d6dca43f053d4345
wlgns12370 <ahffk12370@naver.com>
2024-10-26 17:21:17 +0900

    feat: post 방식으로 회원가입

--- src/main/java/webserver/entity/RequestEntity.java
+++ src/main/java/webserver/entity/RequestEntity.java
@@ -6,23 +6,65 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import util.HttpRequestUtils;
+import util.IOUtils;
+import util.HttpRequestUtils.Pair;
+
 public class RequestEntity {
-    private final HttpMethod httpMessage;
+    private final HttpMethod httpMethod;
     private final String uri;
     private final Map<String, String> queryString;
+    private final Map<String, String> headers;
 
-    private RequestEntity(final HttpMethod httpMessage, final String uri) throws IOException {
-        this.httpMessage = httpMessage;
-        this.uri = uri;
-        this.queryString = null;
-    }
-
-    private RequestEntity(final HttpMethod httpMessage, final String uri, final Map<String, String> queryString) throws IOException {
-        this.httpMessage = httpMessage;
+    private RequestEntity(
+        final HttpMethod httpMethod,
+        final String uri,
+        final Map<String, String> queryString,
+        final Map<String, String> headers
+    ) throws IOException {
+        this.httpMethod = httpMethod;
         this.uri = uri;
         this.queryString = queryString;
+        this.headers = headers;
+    }
+
+    public static RequestEntity from(InputStream in) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        String[] line = br.readLine().split(" ");
+        int index = line[1].indexOf("?");
+
+        List<String> headers = new ArrayList<>();
+        String inputLine;
+
+        while (true) {
+            inputLine = br.readLine();
+            if (inputLine == null || inputLine.equals("")) {
+                break;
+            }
+            headers.add(inputLine);
+        }
+
+        Map<String, String> headerMap = parseHeader(headers);
+        Map<String, String> queryString = null;
+
+        if (HttpMethod.GET.toString().equals(line[0])) {
+            if (hasQueryString(index)) {
+                String requestPath = line[1].substring(0, index);
+                String param = line[1].substring(index+1);
+                queryString = parseQueryString(param);
+                return new RequestEntity(HttpMethod.valueOf(line[0]), requestPath, queryString, headerMap);
+            }
+        }
+        if (HttpMethod.POST.toString().equals(line[0])) {
+            String body = parseBody(br, Integer.parseInt(headerMap.get("content-length")));
+            queryString = parseQueryString(body);
+        }
+        return new RequestEntity(HttpMethod.valueOf(line[0]), line[1], queryString, headerMap);
     }
 
     private static boolean hasQueryString(int index) {
@@ -32,26 +74,25 @@ public class RequestEntity {
         return false;
     }
 
-    public static RequestEntity from(InputStream in) throws IOException {
-        BufferedReader input = new BufferedReader(new InputStreamReader(in));
-        String[] line = input.readLine().split(" ");
-        int index = line[1].indexOf("?");
-        if (hasQueryString(index)) {
-            String requestPath = line[1].substring(0, index);
-            String param = line[1].substring(index+1);
-            Map<String, String> queryString = parseQueryString(param);
+    private static Map<String, String> parseHeader(List<String> headers) {
+        Map<String, String> headerMap = new HashMap<>();
+        headers.forEach(header -> {
+            Pair pair = HttpRequestUtils.parseHeader(header);
+            headerMap.put(pair.getKey(),pair.getValue());
+        });
+        return headerMap;
+    }
 
-            return new RequestEntity(HttpMethod.valueOf(line[0]), requestPath, queryString);
-        }
-        return new RequestEntity(HttpMethod.valueOf(line[0]), line[1]);
+    private static String parseBody(BufferedReader br, int contentLength) throws IOException{
+        return IOUtils.readData(br, contentLength);
     }
 
     public String getUri() {
         return uri;
     }
 
-    public HttpMethod getHttpMessage() {
-        return httpMessage;
+    public HttpMethod getHttpMethod() {
+        return httpMethod;
     }
 
     public Map<String, String> getQueryString() {
